34c37c674ddbfc5dfd3cd653efc4b809cfb09aad
guxukai <1378026744@qq.com>
2019-10-12 11:41:21 +0800

    111

---
+++ deploy/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+WEB_PATH='/github/vue-test'$1
+#WEB_USER='root'
+#WEB_USERGROUP='root'
+
+echo "Start deployment"
+cd $WEB_PATH
+echo "pulling source code..."
+git reset --hard origin/master
+git clean -f
+git pull
+git checkout master
+echo "changing permissions..."
+#chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
+echo "Finished."
