243338203e54a2f0adb37a3fb83eecb0cc25c656
guxukai <1378026744@qq.com>
2019-10-12 13:38:59 +0800

    111

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,15 +1,36 @@
 #!/usr/bin/env bash
-WEB_PATH='/github/vue-test'$1
-#WEB_USER='root'
-#WEB_USERGROUP='root'
+NAME="$1"
+PATH="/github/$1"
+USER="root"
+USERGROUP="root"
 
-echo "Start deployment"
-cd $WEB_PATH
-echo "pulling source code..."
+echo "Start deployment $1"
+cd $PATH
+echo "进入$PATH"
+echo "pulling source code from"
 git reset --hard origin/master
 git clean -f
 git pull
 git checkout master
+if [ $NAME = "vue-test" ]; then
+    echo "delete dist"
+    rm -rf dist
+    echo "build dist"
+    npm run build
+    cp -r dist ../frontend
+fi
+if [ $NAME = "awesome-nest" ]; then
+    echo "delete dist"
+    rm -rf dist
+    echo "build dist"
+    npm run build
+    cp -r dist ../api
+    cp pakage.json ../api
+    npm i
+    docker exec -it api pm2 reload all
+fi
+
+
 echo "changing permissions..."
-#chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
+chown -R $USER:$USERGROUP $PATH
 echo "Finished."
