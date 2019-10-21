d7983845f8caa2e79b32a300b320a3d20b3a37c7
guxukai <1378026744@qq.com>
2019-10-22 07:51:40 +0800

    自动化构建完成

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,12 +5,13 @@ git reset --hard origin/master
 git clean -f
 git pull
 git checkout master
-if [[ ${NAME} = "vue-site" ]]; then
+if [[ $1 == "vue-site" ]]; then
     cnpm i
     npm run build
+    cp -r dist ../html
     dokcer exec -it html nginx -s reload
 fi
-if [[ ${NAME} = "awesome-nest" ]]; then
+if [[ $1 == "awesome-nest" ]]; then
     cnpm i
     npm run build
     dokcer exec -it api pm2 reload
